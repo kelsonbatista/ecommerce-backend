fe0c2b49e6dc30b6a2f72e1174357202a83f362a
Kelson <kelsonbatista@gmail.com>
2024-12-22 18:37:09 -0300

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ativando a validação global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove qualquer propriedade que não esteja no DTO
+      forbidNonWhitelisted: true, // Lança um erro se o DTO tiver propriedades não permitidas
+      transform: true, // Transforma automaticamente os tipos dos dados para o tipo definido no DTO
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
