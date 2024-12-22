6beb278e50c9200aeeef5d9f5b06409d90bbd588
Kelson <kelsonbatista@gmail.com>
2024-12-22 18:39:46 -0300

    feat: add imports and controllers to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { HealthController } from './health/health.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [UsersModule, DatabaseModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
