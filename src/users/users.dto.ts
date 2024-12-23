c9b4e04c1985ee0f0a0ce7972442a46c64e068c7
Kelson <kelsonbatista@gmail.com>
2024-12-23 00:16:57 -0300

    feat: add password matches (commented)

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -21,6 +21,10 @@ export class UserDto {
   @IsNotEmpty()
   @IsString()
   @Length(8, 20)
+  // @Matches(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+  // @Matches(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+  // @Matches(/\d/, { message: 'Password must contain at least one number' })
+  // @Matches(/[@$!%*?&]/, { message: 'Password must contain at least one special character (@$!%*?&)' })
   password: string;
 
   @IsEnum(Role, {
