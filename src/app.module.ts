6ed46882fd4db6bb6684544f831f88a4cde994a5
Kelson <kelsonbatista@gmail.com>
2024-12-26 18:22:15 -0300

    feat: add new modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { HealthController } from './health/health.controller';
 import { UsersModule } from './users/users.module';
+import { StoresModule } from './stores/stores.module';
+import { StoreCategoriesModule } from './store-categories/store-categories.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, StoresModule, StoreCategoriesModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
